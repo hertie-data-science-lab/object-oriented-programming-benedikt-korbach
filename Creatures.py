a81b9e9947020ee98de1679bc4f02e706fac7e01
Benedikt Korbach <benedikt.korbach@hotmail.de>
2023-02-17 12:30:25 +0100

    Collisions and final positions

--- Creatures.py
+++ Creatures.py
@@ -10,16 +10,17 @@ import random
 
 class Creature(metaclass=ABCMeta):
 
-    def __init__(self, name, old_position = 0, new_position = 0): #initiate creature
+    def __init__(self, name, old_position = 0, new_position = 0, final_position = 0): #initiate creature
         self.name = name
         self.old_position = old_position
         self.new_position = new_position
+        self.final_position = final_position
     def move(self):
         return random.randint(-1, 1)
 class Bear(Creature):
     def __init__(self, name = "Bear"):
-        Creature.__init__(self, name, old_position = 0, new_position = 0)
+        Creature.__init__(self, name, old_position = 0, new_position = 0, final_position = 0)
         
 class Fish(Creature):
     def __init__(self, name = "Fish"):
-        Creature.__init__(self, name, old_position = 0, new_position = 0)
+        Creature.__init__(self, name, old_position = 0, new_position = 0, final_position = 0)
--- River.py
+++ River.py
@@ -29,6 +29,7 @@ class River:
             if isinstance(i, Fish) or isinstance(i, Bear):
                 i.old_position = index
                 i.new_position = index
+                i.final_position = index
             index += 1
 
         return self.ecosystem
@@ -38,17 +39,55 @@ class River:
         self.period = self.period + 1
         print("Current period:", self.period)
 
-        index = 0
         for i in self.ecosystem:
             if isinstance(i, Fish) or isinstance(i, Bear):
                 print(i.old_position)
-                i.old_position = i.new_position
+                i.old_position = i.final_position
                 i.new_position = i.old_position + i.move()
                 if i.new_position < 0 or i.new_position > self.n_room:
                     i.new_position = i.old_position
                 print(i.new_position)
 
-            index += 1
+        collision_list = []
+        index_list = []
+        tuple_list = []
+        for i in self.ecosystem:
+            if isinstance(i, Fish) or isinstance(i, Bear):
+                if i.new_position in index_list:
+                    collision_list.append(i.new_position)
+                tuple_list.append((i.new_position, i))
+                index_list.append(i.new_position)
+
+        print("index_list: ", index_list)
+        print("collision_list: ", collision_list)
+        print("tuple_list: ", tuple_list)
+
+        for animal in tuple_list:
+            animal[1].final_position = animal[1].new_position
+
+        for i in collision_list:
+            interaction_list = []
+            for t in tuple_list:
+                if t[0] == i:
+                    interaction_list.append(t[1])
+            print("interaction_list =", interaction_list)
+            if len(set(interaction_list)) == 1:
+                for animal in interaction_list:
+                    animal.final_position = animal.old_position
+            else:
+                for animal in interaction_list:
+                    if isinstance(animal, Bear):
+                        animal.final_position = animal.new_position
+
+        self.ecosystem = seq
+        for i in tuple_list:
+
+
+
+
+
+
+
         #Identify after the creature's movement which are the null positions in the river
         null_positions = []
         for i in range(self.n_room):
